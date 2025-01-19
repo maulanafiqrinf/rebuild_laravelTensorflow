216b9e749fb693c7eb3621535878c58d1445955f
maulana fiqri <fiqrin1805@gmail.com>
2025-01-19 15:57:33 +0700

    perbarui footer dengan elemen baru dan hapus informasi yang tidak diperlukan

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -44,46 +44,30 @@
                             and providing valuable insights tailored to your business.</p>
                         <a href="contact.html" class="theme-btn">Get an appointment now</a>
 
-                        <div class="footer-experience d-flex align-items-center">
-
-                            <div class="footer-experience-item">
-                                <h1>2 <span>Mins</span></h1>
-                                <p>Response Time</p>
+                        <div class="copyright-area">
+                            <div class="custom-container">
+                                <div class="custom-row d-flex align-items-center justify-content-between">
+                                    <ul class="social-links d-flex align-items-center">
+                                        <li><a href="#">
+                                            <i class="iconoir-dribbble"></i>
+                                        </a></li>
+                                        <li><a href="#">
+                                            <i class="iconoir-twitter"></i>
+                                        </a></li>
+                                        <li><a href="#">
+                                            <i class="iconoir-instagram"></i>
+                                        </a></li>
+                                        <li><a href="#">
+                                            <i class="iconoir-linkedin"></i>
+                                        </a></li>
+                                    </ul>
+                    
+                                    <p>&copy; 2025 All rights reserved by <a href="">mfdev87</a></p>
+                                </div>
                             </div>
-                            <div class="footer-experience-item">
-                                <h1>99%</h1>
-                                <p>Client Satisfaction</p>
-                            </div>
-                            <div class="footer-experience-item">
-                                <h1>22+ <span>Years</span></h1>
-                                <p>Field Experience</p>
-                            </div>
-
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="copyright-area">
-        <div class="custom-container">
-            <div class="custom-row d-flex align-items-center justify-content-between">
-                <ul class="social-links d-flex align-items-center">
-                    <li><a href="#">
-                        <i class="iconoir-dribbble"></i>
-                    </a></li>
-                    <li><a href="#">
-                        <i class="iconoir-twitter"></i>
-                    </a></li>
-                    <li><a href="#">
-                        <i class="iconoir-instagram"></i>
-                    </a></li>
-                    <li><a href="#">
-                        <i class="iconoir-linkedin"></i>
-                    </a></li>
-                </ul>
-
-                <p>&copy; 2023 All rights reserved by <a href="https://themeforest.net/user/wordpressriver/portfolio">WordpressRiver</a></p>
             </div>
         </div>
     </div>
