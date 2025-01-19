7e89bb1afa672fb17b00c5cec8acd335ef2bb7b9
maulana fiqri <fiqrin1805@gmail.com>
2025-01-19 18:04:15 +0700

    perbaiki penggunaan huruf kapital pada model Client dan Blog di frontendController

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -4,34 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\models\client;
-use App\Models\tb_site;
 use App\Models\blog;
 
-class FrontendController extends Controller
+class frontendController extends Controller
 {
     public function index(Request $request)
     {
-        $clients = client::where('category', 'client')->get() ?? collect([]);
-        $services = client::where('category', 'service')->get() ?? collect([]);
-        $blogs = blog::where('option', 'visibilty')->get() ?? collect([]);
-        
+        $clients = Client::where('category', 'client')->get();
+        $services = Client::where('category', 'service')->get();
+        $blogs = Blog::where('option', 'visibilty')->get();
         return view('frontend', compact('clients', 'services', 'blogs'));
         
     }
-
-    // public function store(Request $request)
-    // {
-    //     $request->validate([
-    //         'name' => 'required',
-    //         'email' => 'required|email',
-    //         'hp' => 'required',
-    //         'pesan' => 'required',
-    //     ]);
-
-    //     tb_contact::create($request->all());
-
-    //     // Redirect ke homepage dengan pesan sukses
-    //     return redirect()->route('frontend')
-    //         ->with('success', 'Pesan berhasil terkirim.');
-    // }
 }
