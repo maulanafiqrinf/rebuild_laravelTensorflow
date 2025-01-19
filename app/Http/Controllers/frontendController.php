84f811156f204bd2c3c311c4772550c523215f74
maulana fiqri <fiqrin1805@gmail.com>
2025-01-19 13:28:55 +0700

    perbarui vroute

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -11,10 +11,10 @@ class FrontendController extends Controller
 {
     public function index(Request $request)
     {
-        $clients = Client::where('category', 'client')->get();
-        $services = Client::where('category', 'service')->get();
-        $blogs = Blog::where('option', 'visibilty')->get();
-        return view('frontend', compact('clients', 'services', 'blogs'));
+        // $clients = Client::where('category', 'client')->get();
+        // $services = Client::where('category', 'service')->get();
+        // $blogs = Blog::where('option', 'visibilty')->get();
+        return view('frontend');
         
     }
 
