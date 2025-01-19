27d08e1e6a082f5a08c09c08ead05cba93fc8618
maulana fiqri <fiqrin1805@gmail.com>
2025-01-19 17:45:25 +0700

    aktifkan kembali pengambilan data klien, layanan, dan blog di frontend

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -11,9 +11,9 @@ class FrontendController extends Controller
 {
     public function index(Request $request)
     {
-        // $clients = Client::where('category', 'client')->get();
-        // $services = Client::where('category', 'service')->get();
-        // $blogs = Blog::where('option', 'visibilty')->get();
+        $clients = Client::where('category', 'client')->get();
+        $services = Client::where('category', 'service')->get();
+        $blogs = Blog::where('option', 'visibilty')->get();
         return view('frontend');
         
     }
